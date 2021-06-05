6e9ea48d2376d57072ee0d63ef4fb092b183da61
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-06-05 16:51:35 +0530

    check_dict_empty_or_not

---
+++ qns.4.py
@@ -0,0 +1,8 @@
+# Write a Python program to check a dictionary is empty or not.
+
+#info={}
+info={'name':'Ajay singh','sec':'C','address':'GLA university'}
+if info=={}:
+    print('the dict is completely empty')
+else:
+    print('something is in the dict')
