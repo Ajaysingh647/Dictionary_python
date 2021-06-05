dc4c9d761f7a01d1ba11f7edc1f271b1ed41fd3d
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-06-05 16:50:53 +0530

    removing_key

---
+++ qns.3.py
@@ -0,0 +1,6 @@
+# Write a Python program to remove a key from a dictionary.
+
+info={'name':'Ajay singh','sec':'C','address':'GLA university'}
+print('The info before removing the keys:-',info)
+d=info.pop('address')
+print('after removing the keys address:-',info)
