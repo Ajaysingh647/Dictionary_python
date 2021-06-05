2c8d027f776c4ca05a5c774fcc756c01bc62e121
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-06-05 16:52:18 +0530

    sorting_by_key

---
+++ qns.6.py
@@ -0,0 +1,6 @@
+#  Write a Python program to sort a dictionary by key.
+
+info={'name':'Ajay singh','sec':'C','address':'GLA university'}
+print('before sorting keys of dict',info)
+s=sorted(info)
+print('after sorting keys of dict',s)
